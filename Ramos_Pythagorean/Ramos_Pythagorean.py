6742bb9592ac7931e098773912735398e1877588
Adrian Ramos <adrianram.31598@redlandsschools.net>
2018-09-14 15:25:19 +0000

    new pythagorean

---
+++ Ramos_Pythagorean/Ramos_Pythagorean.py
@@ -0,0 +1,5 @@
+name = input('Hello! What is your name? ')
+print("Pleasure meeting you " + (name) + ". I hope you're enjoying your day! My name is Python III.")
+print('Today, I will be helping you solve a triangle using the Pythagorean Theorem.')
+
+print("First, I'm going to ask you to enter the lengths of the legs a and b of a right triangle")
---
+++ warmup.py
@@ -0,0 +1,8 @@
+
+number = int(input ('Pick a whole number: '))
+if (number%2==0) : 
+    print ('The number ' + str(number) + ( ' is even.'))
+    
+elif (number%2==1) :
+    print ('The number ' + str(number) + (' is odd.'))
+    
