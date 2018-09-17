0c3dd1c9cd75083bedb6e20aa6e25ada13ce6d8b
Adrian Ramos <adrianram.31598@redlandsschools.net>
2018-09-17 15:23:49 +0000

    added amazing file for challenges

--- Ramos_Pythagorean/Ramos_Pythagorean.py
+++ Ramos_Pythagorean/Ramos_Pythagorean.py
@@ -1,6 +1,6 @@
 name = input('Hello! What is your name? ')
 
-print("Pleasure meeting you " + (name) + ". I hope you're enjoying your day! My name is Python III.")
+print("Pleasure meeting you " + name + ". I hope you're enjoying your day! My name is Python III.")
 print('Today, I will be helping you solve a right triangle using the Pythagorean Theorem.')
 
 print("First, I'm going to ask you to enter the lengths of the legs 'a' and 'b' of a right triangle.")
---
+++ amazing.py
@@ -0,0 +1,19 @@
+"""
+This program demonstrates IMPORT statements and LOOPS 
+"""
+
+# import statements go FIRST! (TOP OF YOUR CODE!)
+import time
+
+# for variableName in range(start, stop, step):
+# OR
+# for variableName in range(stop): *This will start at 0 and step by 1
+
+# Count to 100 by 5's
+'''for a in range (0, 101, 5):
+    print ('Hello ' + str(a))
+'''
+    
+# Count down from (START AT 20) by 2's and stop at -4
+for b in range (20, -5, -2):
+    print('Hello ' + str(b))
