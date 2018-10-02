12ab1625a2ffa7414bdce29f446ea9d2aaf4107a
Adrian Ramos <adrianram.31598@redlandsschools.net>
2018-10-02 16:03:35 +0000

    practically finished the story

--- adventurestory.py
+++ adventurestory.py
@@ -1,7 +1,7 @@
 import time
+name = input("To begin our journey, what is your name? ")
 
 def greeting():
-    name = input("To begin our journey, what is your name? ")
     print("Salutations " + name + ".")
     print("Your story begins on a perfectly average Sunday and you, " + name + ", are a perfectly average person...")
     time.sleep(2)
@@ -13,30 +13,41 @@ def greeting():
     actDepression()
 
 def actDepression():
-    decision1 = int(input("Do you act upon your craving or reppress it like all other emotions in your life? Type '1' for act or '2' for reppress: "))
+    decision1 = int(input("Well, " + name + ", do you act upon your craving or reppress it like all other emotions in your life? Type '1' for act or '2' for reppress: "))
     if (decision1 == 1): 
         print("Because of your craving, you decide to go the store.")
+        atStore()
     else:
         print("Reconsider your life choices. You will have to go to the store like a functioning human.")
         actDepression()
-    atStore()
+    
     
 def atStore():
-    print("As you amble your way to the dairy section...something is wrong. Row after row of empty milk shelves approach you. You must make a decision ")
-    decision2 = input("Do you ask an associate if there is any milk in the back room or do you have a tantrum? Type '1' for ask or '2' for tantrum: ")
+    print("As you amble your way to the dairy section...something is wrong. Row after row of empty milk shelves approach you. You must make a decision. ")
+    decision2 = int(input("Do you ask an associate if there is any milk in the back room or do you have a tantrum? Type '1' for ask or '2' for tantrum: "))
     if (decision2 == 1):
         print("You ask an associate and he tells you to follow him to the storage room.")
+        soyOr2percent()
     else:
         print("Are you done? Pick yourself off the floor and use those defunct social skills.")
         atStore()
         
 def soyOr2percent():
     print("The associate leads you to an unmarked door at the back of the store. He unlocks the door, revealing a massive warehouse filled with...milk? At last, the end is near and the milk is here. But the associate is now attempting to get your attention. He asks you a question: 'So you want soy or 2%?' ")
-    print("Type '1' for ask or '2' for tantrum:")
+    decision3 = int(input("Type '1' for soy or '2' for 2%:"))
+    if (decision3 == 1):
+        print("Compras la botella de leche y luego vuelves a tu casa. Pones la botella de leche arriba del mostrador, y buscas por el cereal. Encuentras la caja de Frosted Flakes. Sacudes la caja... y una escama solitaria se cae de la caja. La vida no tiene sentido.")
+    elif (decision3 == 2):
+        print("You buy the bottle of milk and return home. You set your hard-won milk on the kitchen counter, now searching for the cereal. You find the box of Frosted Flakes. You shake out the box...and one solitary flake falls out. Life is meaningless. ")
+    else:
+        print("Try again. Intentalo de nuevo.")
+        soyOr2percent()
+
+
     
     
         
 greeting()
-atStore()
+
 
 
