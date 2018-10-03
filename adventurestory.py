23749d99dea7483d9e090d2b2ab8c6af1fa58fc1
Adrian Ramos <adrianram.31598@redlandsschools.net>
2018-10-03 15:24:20 +0000

    almost finished story

--- adventurestory.py
+++ adventurestory.py
@@ -1,53 +1,73 @@
 import time
+import sys
+
 name = input("To begin our journey, what is your name? ")
 
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.095)
+
 def greeting():
-    print("Salutations " + name + ".")
-    print("Your story begins on a perfectly average Sunday and you, " + name + ", are a perfectly average person...")
-    time.sleep(2)
-    print("However at precisely 7:36PM, something changes. Thoughts of Frosted Flakes consume your mind, everything has narrowed down to the prospect of one (1) bowl of cereal.")
-    print("You decide to power walk down to your kitchen.")
-    print("You find the cereal, the bowl, the spoon but the time comes to seek the milk...")
-    time.sleep(2)
-    print("It's not there? How could this be? You're out of milk.")
-    actDepression()
+        delay_print("Salutations " + name + ".\nYour story begins on a perfectly average Sunday and you, " + name + ", are a perfectly average person...")
+        time.sleep(0.75)
+        delay_print("however at precisely 7:36PM, something changes.\nThoughts of Frosted Flakes consume your mind, everything has suddenly narrowed down to the prospect of one (1) bowl of cereal.\nYou make your way to the kitchen.\nThe cereal, the bowl, and the spoon are all easy to find but when the time comes to seek the milk...")
+        time.sleep(1)
+        delay_print("\n...It's not there?")
+        time.sleep(1)
+        delay_print("\nHow could this be?")
+        time.sleep(1)
+        delay_print("\nYou're out of milk.")
+        actDepression()
 
 def actDepression():
-    decision1 = int(input("Well, " + name + ", do you act upon your craving or reppress it like all other emotions in your life? Type '1' for act or '2' for reppress: "))
-    if (decision1 == 1): 
-        print("Because of your craving, you decide to go the store.")
-        atStore()
-    else:
-        print("Reconsider your life choices. You will have to go to the store like a functioning human.")
+    time.sleep(1.5)
+    decision1 = int(input("\nWell, " + name + ", do you continue to act upon your cereal craving or reppress it like all other emotions in your life? Type '1' for act or '2' for reppress: "))
+    if (decision1 == 1):
+        delay_print("Good choice. You find your keys and hightail it to the nearest grocery store.")
+    elif (decision1 == 2):
+        delay_print("Reconsider your life choices. You will have to go to the store like a functioning human.")
         actDepression()
-    
+    else:
+        delay_print("Listen up smarty. I said to pick either 1 or 2. Let's try it again.")
+        actDepression()
+    time.sleep(1)
+    delay_print("\n\nYou've made it to Generic Grocery Store.\nAs you amble your way towards the dairy section, you realize something is very wrong.\nRow after row of empty dairy shelves greet you. If you want that bowl of cereal, you must make a decision. ")
+    atStore()
     
 def atStore():
-    print("As you amble your way to the dairy section...something is wrong. Row after row of empty milk shelves approach you. You must make a decision. ")
-    decision2 = int(input("Do you ask an associate if there is any milk in the back room or do you have a tantrum? Type '1' for ask or '2' for tantrum: "))
+    decision2 = int(input("\nDo you ask an associate about the missing milk or do your emotions get the best of you and throw a tantrum? Type '1' for ask or '2' for tantrum: "))
     if (decision2 == 1):
-        print("You ask an associate and he tells you to follow him to the storage room.")
+        delay_print("You ask an associate. He nods and seems to understand your question.\nHe wordlessly motions you to follow him to the back of the store. ")
         soyOr2percent()
-    else:
-        print("Are you done? Pick yourself off the floor and use those defunct social skills.")
+    elif (decision2 == 2):
+        delay_print("Are you done?")
+        time.sleep(1)
+        delay_print(" Pick yourself up off the floor and use those defunct social skills.")
         atStore()
-        
-def soyOr2percent():
-    print("The associate leads you to an unmarked door at the back of the store. He unlocks the door, revealing a massive warehouse filled with...milk? At last, the end is near and the milk is here. But the associate is now attempting to get your attention. He asks you a question: 'So you want soy or 2%?' ")
-    decision3 = int(input("Type '1' for soy or '2' for 2%:"))
-    if (decision3 == 1):
-        print("Compras la botella de leche y luego vuelves a tu casa. Pones la botella de leche arriba del mostrador, y buscas por el cereal. Encuentras la caja de Frosted Flakes. Sacudes la caja... y una escama solitaria se cae de la caja. La vida no tiene sentido.")
-    elif (decision3 == 2):
-        print("You buy the bottle of milk and return home. You set your hard-won milk on the kitchen counter, now searching for the cereal. You find the box of Frosted Flakes. You shake out the box...and one solitary flake falls out. Life is meaningless. ")
     else:
-        print("Try again. Intentalo de nuevo.")
+        delay_print("Look, either '1' or '2,' pick one.")
+        atStore()
+    delay_print("The associate leads you to the unmarked door of a storage room.\nHe unlocks the door, revealing a massive refrigerated warehouse filled with crates of...milk?\nAt last, the end is near and the milk is here.\nThe associate attempts to get your attention again. He asks you a question:\n'So you want soy or 2%?'")
+    soyOr2percent()
+    
+def soyOr2percent():
+    decision3 = int(input("\nType '1' for soy or '2' for 2%: "))
+    if (decision3 == 1):
+        delay_print("Compras la botella de leche y luego vuelves a tu casa.\nPones la botella de leche arriba del mostrador, y buscas por el cereal.\nEncuentras la caja de Frosted Flakes. Sacudes la caja...")
+        time.sleep(1.5)
+        delay_print("\n\n...una escama solitaria se cae de la caja...")
+        time.sleep(2)
+        delay_print("\n\n\nLa vida no tiene sentido.")
+    elif (decision3 == 2):
+        delay_print("You buy the bottle of milk and return home.\nYou set your hard-won milk on the kitchen counter, now searching for the cereal.\nYou find the box of Frosted Flakes and shake it over your empty bowl...")
+        time.sleep(1.5)
+        delay_print("\n\n...one solitary flake falls out...")
+        time.sleep(2)
+        delay_print("\n\n\nLife is meaningless.\n")
+    else:
+        delay_print("Try again.\nIntentalo de nuevo.")
         soyOr2percent()
-
-
     
-    
-        
 greeting()
-
-
-
